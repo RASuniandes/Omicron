1321cb8cc1e099c438b174fd7227f0d478a5be94
FranklinSRomero <113615053+FranklinSRomero@users.noreply.github.com>
2023-10-20 09:13:15 -0500

    ultimos cambios

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,9 +1,6 @@
 #include <Arduino.h>
-#include "Qtr.h"
 #include "Motor.h"
-#include "Sharp.h"
-#include "Starter.h"
-#include "Pid.h"
+
 #include "BluetoothSerial.h"
 BluetoothSerial SerialBT;
 
@@ -14,83 +11,15 @@ BluetoothSerial SerialBT;
 
 int command;
 int Start;
-int Accelerator;
+int xd= 83;
 
-//Qtr - Blanco/negro
-Qtr qtr_left(4);
-Qtr qtr_right(25);
-
-int qtr_left_value;
-int qtr_right_value;
-
-// Sharps sensors
-Sharp s1(18, 0); //Sensor_1
-Sharp s2(19, 1); //Sensor_2
-Sharp s3(13, 2); //Sensor_3
-Sharp s4(23, 3); //Sensor_4
-Sharp s5(5, 4); //Sensor_5
 
 //Motores
 Motor motor_b(27,26);
 Motor motor_a(33,32);
-
-
-
-//Starter 
-Starter start(21,14);
-
-//Comentarios
-bool comments= true;
-
-//Otras Variables auxiliares
-int _numSensors=5; //cantidad de sensores usados
-int sensorValues[5];
  
-//PID
-unsigned long time_motores = 0;
-bool get_in = false;
-
-int time_backward=600; // Constante de tiempo reversa  total
-int time_backward_twist=400; // constante de tiempo del giro por lo tato menor a time_backward
-int velocity_backward=150;
-
-
-
-// float L=0.01;
-// float T=0.1;
-// float K=2.5;  
-
-int reference = 250;
-int z=20;
-// float Kp = 1.2*(T/(K*L)); //1.2 *(T)/KL
-// float Ki = 2*L; //2L
-// float Kd =0.5*L;//0.5L
-// float Kp = 22.5;
-// float Ki = 0;
-//float Kd = 2.5;
-float Kp = 10;
-float Ki = 0;
-float Kd = 0;
-
-Pid pid(Kp, Ki, Kd, 20, reference, _numSensors);
-
-
-int* getSharpValues() {
-
-  sensorValues[0] = s1.readValue();
-  sensorValues[1] = s2.readValue();
-  sensorValues[2] = s3.readValue();
-  sensorValues[3] = s4.readValue();
-  sensorValues[4] = s5.readValue();  
-  return sensorValues;
-}
 
 void motores(int speed_m_left, int speed_m_right){
-  //Serial.print("motorA SPEED: ");
-  //Serial.println(speed_m_left);
-
-  //Serial.print("motorB SPEED: ");
-  //Serial.println(speed_m_right);
 
   motor_a.setSpeed(speed_m_left);
   motor_b.setSpeed(speed_m_right);
@@ -98,196 +27,53 @@ void motores(int speed_m_left, int speed_m_right){
 }
 
 
-void show_sensors(){
-  Serial.print("qtr_lef: ");
-  Serial.print(qtr_left_value);
-  Serial.print("  qtr_right: ");
-  Serial.print(qtr_right_value);
-  Serial.print(" s1: ");
-  Serial.print(s1.readValue());
-  Serial.print(" s2: ");
-  Serial.print(s2.readValue());
-  Serial.print(" s3: ");
-  Serial.print(s3.readValue());
-  Serial.print(" s4: ");
-  Serial.print(s4.readValue());
-  Serial.print(" s5: ");
-  Serial.println(s5.readValue());
-  Serial.print("Starter: ");
-  Serial.println(start.getStart());
-  Serial.print("Stop: ");
-  Serial.println(start.getStop());
-  Serial.print("Stop: \n\n\n\n\n\n\n\n");
-
-}
-
-
-void Twist_reverse(int direction){
-
-  // si direction es igual 0 es qtr_left
-  int motorA_velocity =0;
-  int motorB_velocity =0;
-  if (direction==1){
-
-    motorA_velocity=-velocity_backward-40;
-    motorB_velocity=-velocity_backward;
-
-  }
-  else{
-    
-    motorA_velocity=-velocity_backward;
-    motorB_velocity=-velocity_backward-40;
-
-  }
-
-  unsigned long startTime = millis();
-  while (millis() - startTime < time_backward ){
-
-
-      if (millis() - startTime < (time_backward-time_backward_twist)) {
-        Serial.println("atras");
-        
-        motores(-250,-250);
-      // Código para el primer intervalo de reversa
-      } else {
-        // Código para el intervalo restante de giro en segundos
-        Serial.println("giro");
-        motores(motorA_velocity,motorB_velocity);
-
-      }  
-    }
-}
-
-
-void getIn(int qtr_left_value, int qtr_right_value){
-
-  if (qtr_right_value == 0 && qtr_left_value ==1) {
-    //Serial.println("qtr_right");
-    Twist_reverse(1); 
-  }
-        
-  else if (qtr_right_value == 1 && qtr_left_value ==0) {
-    //Serial.println("qtr_left");
-    Twist_reverse(0);
-  }
-
-  else if (qtr_right_value == 0 && qtr_left_value == 0){
-
-    //Serial.println("qtr_left && qtr_right");
-    Twist_reverse(1);
-      
-  } else {
-    //Serial.println("Caso xd que nunca pasa pero pos mejo");
-    Twist_reverse(1);
-  }
-}
-
-
-void tracking(){
-  int* values = getSharpValues();
-  int salida_control = pid.traking(values);    
-  
-
-  if(salida_control<0) { 
-
-    motores( salida_control, reference );
-
-  } else if(salida_control>0) { 
-    motores(reference, -salida_control);
-
-  } else {  
-    motores(255 , 255);
-    
-  }
-}
-
-void gameStart(){
-
-  qtr_left_value = qtr_left.value();
-  qtr_right_value = qtr_right.value();
-
-  if (qtr_left_value==1 && qtr_right_value==1){
-    
-    tracking();
-    //motores(40,40);
-    
-
-  } else{
-    //Serial.println("Get in");
-    getIn(qtr_left_value, qtr_right_value);
-   
-   
- 
-  } 
-}
-
 void setup() {
   // put your setup code here, to run once:
-
-    if ( comments == true){
-      Serial.begin(9600);
-    } 
     Serial.begin(9600);
-    SerialBT.begin("ESP32test"); //Bluetooth device name
+    SerialBT.begin("ESP32_test_prod"); //Bluetooth device name
     Serial.println("The device started, now you can pair it with bluetooth!");
      
 }
 
-void executecommand(int command,int Accelerator){
-  Serial.print(command);
-
-  if (command == 1){
-    motores(83*(Accelerator),83*Accelerator);
+void executecommand(int command){
+    Serial.print(command);
+    Serial.print(xd);
+  if (command == 70){  //pa adelante
+    motores(xd,xd);
+    //Serial.print("adelante");
     }
-  else if (command == 2){
-    motores(0,83*(Accelerator));
+  else if (command == 76){ //pa izquierda 
+    motores(-xd,xd);
     }
-  else if (command == 3){
-    motores(83*(Accelerator),0);
+  else if (command == 71){ //pa diag izquierda
+    motores(-xd/2,xd);
+    } 
+  else if (command == 82){ //pa derecha
+    motores(xd,-xd);
     }
-  else if (command == 4){
-    motores(-83*(Accelerator),-83*(Accelerator));
+  else if (command == 73){ //pa diagderecha
+    motores(xd,-xd/2);
     }
-  else if(command == 5){
+  else if (command == 66){ //pa atras
+    motores(-xd,-xd);
+    }
+  else if(command == 83){
     motores(0,0);}
-  //else if(command == 9){
-    //Start = 1;}
-  //else if(command == 0){
-    //Start = 0;}
-  SerialBT.print("Accelerator: ");
-  SerialBT.print(Accelerator);
-  SerialBT.print("  command: ");
-  SerialBT.print(command);
- 
+  else if(command == 49){
+    xd=0;}
+  else if(command == 53){
+    xd=83;}
+  else if(command == 56){
+    xd=180;}
+  else if(command == 1133535){
+    xd=255;}
 }
 
-void desiciones(){
-  if (Start==1){
-  
-    gameStart();
-    //tracking();
-    //motores(255, 255);
-    
 
-  } else {
-    motores(0,0);
-  }
-}
 void loop() {
   if (SerialBT.available()) {
     command = SerialBT.read();
-    command-=48;
-    if (command > 5){
-      Accelerator = command;
-      Accelerator-=5;
-    }
-    executecommand(command,Accelerator);
-    Serial.print(command);
+    Serial.print("\n");
+    
+    executecommand(command);}
   }
-  
-  //desiciones();
-
-  //show_sensors();
-
-
-}
